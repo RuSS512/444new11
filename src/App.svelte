5de20bc83860f0259e1c9c5771bcd540cb3607dd
RuSS512 <vip.rusel2@gmail.com>
2022-01-28 00:23:57 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -16,10 +16,10 @@
 
   /***********************************/
   // Customise the app by changing the following variables.
-  const TITLE = "Shapes";
-  const DESCRTIPTION = "A collection of shapes on the blockchain";
-  const HEADER_TITLE = "shapes.xyz";
-  const HEADER_LINK = "https://lanablocks.xyz";
+  const TITLE = "SOLETTERS NFT";
+  const DESCRTIPTION = "The world's FIRST collection of GameFi NFT letters, with DeFi utility!";
+  const HEADER_TITLE = "soletters.io";
+  const HEADER_LINK = "https://soletters.io";
   // Your image or GIF needs to be in the /public folder for this to work
   const IMAGE_LINK = "/example.gif";
   /***********************************/
